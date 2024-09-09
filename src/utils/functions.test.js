25e938c20b775556b03ff347f66893c3147ce252
Erick244 <erienrique03@gmail.com>
2024-09-09 18:14:43 -0300

    test(functions): subtraction test

--- src/utils/functions.test.js
+++ src/utils/functions.test.js
@@ -1,5 +1,9 @@
-import { sum } from "./functions";
+import { subtraction, sum } from "./functions";
 
 test("sum", () => {
 	expect(sum(1, 2)).toBe(3);
+})
+
+test("subtraction", () => {
+	expect(subtraction(2, 1)).toBe(1);
 })
