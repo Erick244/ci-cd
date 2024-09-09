c8bd1eed6314ad06fc0f0ecfefca6595c4110047
Erick244 <erienrique03@gmail.com>
2024-09-09 18:14:21 -0300

    feat(subtraction): add subtraction

--- src/script.js
+++ src/script.js
@@ -1,10 +1,15 @@
-import { sum } from "./utils/functions.js";
+import { subtraction, sum } from "./utils/functions.js";
 
 const calculate = () => {
-	const x = +document.getElementById("number1").value;
-	const y = +document.getElementById("number2").value;
+	const sum_x = +document.getElementById("sum1").value;
+	const sum_y = +document.getElementById("sum2").value;
 
-	document.getElementById("result").textContent = sum(x, y);
+	document.getElementById("sum_result").textContent = sum(sum_x, sum_y);
+
+	const sub_x = +document.getElementById("sub1").value;
+	const sub_y = +document.getElementById("sub2").value;
+
+	document.getElementById("sub_result").textContent = subtraction(sub_x, sub_y);
 }
 
 const submitButton = document.getElementById("submit");
